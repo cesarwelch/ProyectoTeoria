2f3f61b1fef23933eb8b5981a3c14c375864de72
Cesar <cesar777welch@gmail.com>
2016-12-17 12:13:19 -0600

    Helper TM

--- app/helpers/tm_helper.rb
+++ app/helpers/tm_helper.rb
@@ -1,2 +1,15 @@
 module TmHelper
+	class TM < Parent::Parent
+		attr_accessor :inputAlphabet, :tapeAlphabet, :tape, :reject
+
+		def initialize(elements={})
+			yaml = {}
+			yaml = YAML::load_file(elements[:file]) if elements.has_key? :file
+			super(elements)
+			@inputAlphabet = elements[:inputAlphabet] || yaml['inputAlphabet']
+			@tapeAlphabet = elements[:tapeAlphabet] || yaml['tapeAlphabet']
+			@tape = TMTape.new
+			@accept = false
+			@reject = false
+		end
 end
