51ecb8527985ef92ac746180f105bef94bfedfb6
DanielaOdileth <odilethpadilla@unitec.edu>
2016-12-17 14:53:22 -0600

    transiciones del pda

--- app/helpers/pda_helper.rb
+++ app/helpers/pda_helper.rb
@@ -15,10 +15,57 @@ module PdaHelper
       resp[:accept]
     end
 
-    #Metodo transiciones
-    def transition()
+    def transition(state, symbol, stackTop=nil)
+      dests = []
+
+			if has_transition?(state, symbol)
+				actions = @transitions[state][symbol]
+				stackTop ||= @stack.last
+				able = true
+				@stack.push actions['push'] if actions['push']
+				if actions['pop']
+					able = false unless stackTop == actions['pop']
+					@stack.pop if able
+				end
+
+        if able
+					dests << actions['to']
+
+					if has_transition?(actions['to'], '&')
+						actions = @transitions[actions['to']]['&']
+						able = true
+						@stack.push actions['push'] if actions['push']
+						if actions['pop']
+							able = false unless @stack.last == actions['pop']
+							@stack.pop if able
+					  end
+					if able
+							dests << actions['to']
+					end
+				end
+          dests
+        else
+          return dests
+        end
+        else
+          return []
+        end
+      end
     end
 
+    #metodo pop
+
+    #metodo has_transition
+    def has_transition?(state, symbol)
+      return false unless @transitions.has_key? state
+      if @transitions[state].has_key? symbol
+        actions = @transitions[state][symbol]
+        return false if actions['pop'] && @stack.last != actions['pop']
+        return true
+      else
+        return false
+      end
+    end
 
   end
 end
