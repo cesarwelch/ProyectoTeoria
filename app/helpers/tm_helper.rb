9552dab7a94080bdcba97f23a8b4c45ef34778f6
Cesar <cesar777welch@gmail.com>
2016-12-17 12:13:59 -0600

    consumo de String

--- app/helpers/tm_helper.rb
+++ app/helpers/tm_helper.rb
@@ -12,4 +12,33 @@ module TmHelper
 			@accept = false
 			@reject = false
 		end
+
+		def feed(input) 
+			@tape = TMTape.new(input)
+			@accept = false
+			@reject = false
+			movements = []
+			stateHead = @start.to_s
+			movements.push({state: stateHead, via: "-"})
+			input.each_char do |symbol|
+				toState = transition(stateHead, symbol)
+				movements.push({state: toState, via: symbol})
+				if @accept || @reject
+					break
+				else
+					stateHead = toState
+				end
+			end
+		      
+			resp = {
+				input: input,
+				movements: movements,
+				accept: @accept,
+				reject: @reject,
+				head: stateHead,
+				tape: @tape.storage,
+				output: @tape.output
+			}
+			resp
+		end
 end
