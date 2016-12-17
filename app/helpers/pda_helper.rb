3d8e773a74a7b6add5481f6c2d9ec4fbf1dd81cb
DanielaOdileth <odilethpadilla@unitec.edu>
2016-12-17 12:58:21 -0600

    Pda helper

---
+++ app/helpers/pda_helper.rb
@@ -0,0 +1,24 @@
+module PdaHelper
+
+  class PDA < NfaHelper::NFA
+    attr_accessor :stack_user
+
+    def initialize(params={})
+      super(params)
+      @alphabet << '&' unless !@alphabet || @alphabet.include?('&')
+      @stack = []
+      @stack_user = params['stack_user']
+		end
+
+    def accepts?(input)
+      resp = consume(input)
+      resp[:accept]
+    end
+
+    #Metodo de transiciones
+    def transition()
+    end
+
+     
+  end
+end
