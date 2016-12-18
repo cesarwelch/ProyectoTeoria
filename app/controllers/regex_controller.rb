e2b42a0479dfe67ce0902bc5d8777cfaeb6e50d8
Jorge Caballero <j@caballeromurillo.com>
2016-12-18 15:40:52 -0600

    Starting the regex converter..

--- app/controllers/regex_controller.rb
+++ app/controllers/regex_controller.rb
@@ -1,2 +1,16 @@
+require 'json'
+
 class RegexController < ApplicationController
+
+	def index
+
+	end
+
+	def compute
+		hash = RegexHelper::FSM.convert_from_fsm(params['data'])
+        hash = JSON.parse(hash) if hash.is_a?(String)
+        states = hash['states']
+
+	end
+
 end
--- app/helpers/regex_helper.rb
+++ app/helpers/regex_helper.rb
@@ -9,44 +9,70 @@ module RegexHelper
 			states = Set.new
 			transitions = []
 			start = nil
-
+			parent = []
 			element['nodes'].each do |node|
 				states.add(node['text'])
 				if node['isAcceptState']
 					accept.add(node['text'])
 				end
 			end
-			element['links'].each do |link|
-				link['text'].split(',').each do |c|
-					if link['type'] == 'Link'
-						from = element['nodes'][link['nodeA']]['text']
-						to = element['nodes'][link['nodeB']]['text']
-						with = c
-					elsif link['type'] == 'SelfLink'
-						from = element['nodes'][link['node']]['text']
-						to = element['nodes'][link['node']]['text']
-						with = c
+			parent_str = ""
+			states.each_with_index do |state, state_index|
+				pointing_set = []
+				pointing_str = ""
+				symbol_count = 0
+				element['links'].each_with_index do |link, link_index|
+					if symbol_count == 0
+						symbol = ""
+					else
+						symbol = "+"
 					end
-					if link['type'] != 'StartLink'
-						alphabet.add(c)
-						transitions.push({
-							"current_state": from,
-							"symbol": with,
-							"destination": to
-						})
+
+
+					if link['type'] == 'SelfLink' and link['node'] == state_index
+						pointing_str = "#{pointing_str}#{symbol}#{element['nodes'][link['node']]['text']}#{link['text']}"
+						symbol_count += 1
+					else
+						if link['nodeB'] == state_index
+							pointing_str = "#{pointing_str}#{symbol}#{element['nodes'][link['nodeA']]['text']}#{link['text']}"
+							symbol_count += 1
+						end
 					end
 				end
-				if link['type'] == 'StartLink'
-					start = element['nodes'][link['node']]['text']
-				end
+				parent.push({"state": state, "pointing_set": pointing_str})
 			end
-			return {
-				"alphabet": alphabet.to_a,
-				"accept": accept.to_a,
-				"states": states.to_a,
-				"transitions": transitions,
-				"start": start
-			}.to_json
+			return parent
+			# element['links'].each do |link|
+			# 	link['text'].split(',').each do |c|
+			# 		if link['type'] == 'Link'
+			# 			from = element['nodes'][link['nodeA']]['text']
+			# 			to = element['nodes'][link['nodeB']]['text']
+			# 			with = c
+			# 		elsif link['type'] == 'SelfLink'
+			# 			from = element['nodes'][link['node']]['text']
+			# 			to = element['nodes'][link['node']]['text']
+			# 			with = c
+			# 		end
+			# 		if link['type'] != 'StartLink'
+			# 			alphabet.add(c)
+			# 			transitions.push({
+			# 				"current_state": from,
+			# 				"symbol": with,
+			# 				"destination": to
+			# 			})
+			# 		end
+			# 	end
+			# 	if link['type'] == 'StartLink'
+			# 		start = element['nodes'][link['node']]['text']
+			# 	end
+			# end
+			# return {
+			# 	"alphabet": alphabet.to_a,
+			# 	"accept": accept.to_a,
+			# 	"states": states.to_a,
+			# 	"transitions": transitions,
+			# 	"start": start
+			# }.to_json
 		end
 	end
 
