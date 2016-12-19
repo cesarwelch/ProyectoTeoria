d319b2dbfb185615f72348f158a542083c754ff5
DanielaOdileth <odilethpadilla@unitec.edu>
2016-12-18 21:32:52 -0600

    controller tm

--- app/controllers/tm_controller.rb
+++ app/controllers/tm_controller.rb
@@ -42,7 +42,6 @@ class TmController < ApplicationController
       nodes = []
       edges = []
 
-      #aqui
       @tm.states.each do |s|
           n = { data: { id: s }}
           nodes.push(n)
@@ -69,6 +68,56 @@ class TmController < ApplicationController
           edges: edges
         }.to_json.html_safe
 
+    end
+
+    def consume
+        hash = tm_params
+        hash = JSON.parse(hash) if hash.is_a?(String)
+
+        @tm = TmHelper::TM.new
+        @tm.states = hash['states'].split(' ')
+        @tm.start = hash['start']
+        @tm.transitions = {"A"=>{"0"=>{"to"=>"A", "write"=>0, "move"=>"R"}, "1"=>{"to"=>"B", "write"=>1, "move"=>"R"}}, "B"=>{"1"=>{"to"=>"B", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"C", "write"=>0, "move"=>"R"}}, "C"=>{"1"=>{"to"=>"ACCEPT", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"A", "write"=>0, "move"=>"R"}}}
+        @tm.inputAlphabet = hash['inputAlphabet'].split(' ')
+        @tm.tapeAlphabet = hash['tapeAlphabet'].split(' ')
+
+        @compute = @tm.feed(hash['input_string'])
+
+         nodes = []
+        edges = []
+
+        @tm.states.each do |s|
+            n = { data: { id: s }}
+            nodes.push(n)
+        end
+
+        i = 1
+        @tm.transitions.each do |keyt, valt|
+          valt.each do |key, val|
+            e = { data: {
+              id: i.to_s,
+              source: keyt,
+              loqueron: key,
+              move: val['move'],
+              target: val['to'],
+              label: "#{key} -> #{val['write'] ? val['write'] : '&'}, #{val['move']}"
+                }
+            }
+            edges.push(e)
+            i = i+1
+          end
+        end
+
+      @bringElements = {
+        nodes: nodes,
+        edges: edges
+      }.to_json.html_safe
+
+    end
+
+    private
+      def tm_params
+      params.permit(:states, :alphabet, :start, :transitions, :input_string, :inputAlphabet, :tapeAlphabet)
     end
 
 end
