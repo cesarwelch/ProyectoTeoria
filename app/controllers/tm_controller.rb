c235ea043ec2faedab487475bcc13db31992d599
DanielaOdileth <odilethpadilla@unitec.edu>
2016-12-18 21:29:39 -0600

    controller tm

--- app/controllers/tm_controller.rb
+++ app/controllers/tm_controller.rb
@@ -36,51 +36,39 @@ class TmController < ApplicationController
             })
           end
       end
-      #aqui
-    end
 
       @tm.transitions = trans_map
-      # {
-      #   "A" => {
-      #     "0" => {
-      #       "to"=>"A",
-      #       "write"=>0,
-      #       "move"=>"R"
-      #       },
-      #     "1" => {
-      #       "to"=>"B",
-      #       "write"=>1,
-      #       "move"=>"R"
-      #     }
-      #   },
-      #   "B" => {
-      #     "1" => {
-      #       "to"=>"B",
-      #       "write"=>1,
-      #       "move"=>"R"
-      #     },
-      #     "0" => {
-      #       "to"=>"C",
-      #       "write"=>0,
-      #       "move"=>"R"
-      #     }
-      #   },
-      #   "C" => {
-      #     "1" => {
-      #       "to"=>"ACCEPT",
-      #       "write"=>1,
-      #       "move"=>"R"
-      #     },
-      #     "0" => {
-      #       "to"=>"A",
-      #       "write"=>0,
-      #       "move"=>"R"
-      #     }
-      #   }
-      # }
-
 
       nodes = []
       edges = []
+
+      #aqui
+      @tm.states.each do |s|
+          n = { data: { id: s }}
+          nodes.push(n)
+      end
+
+      i = 1
+      @tm.transitions.each do |keyt, valt|
+        valt.each do |key, val|
+          e = { data: {
+            id: i.to_s,
+            source: keyt,
+            move: val['move'],
+            target: val['to'],
+            label: "#{key} -> #{val['write'] ? val['write'] : '&'}, #{val['move']}"
+              }
+          }
+          edges.push(e)
+          i = i+1
+        end
+      end
+
+      @bringElements = {
+          nodes: nodes,
+          edges: edges
+        }.to_json.html_safe
+
+    end
 
 end
